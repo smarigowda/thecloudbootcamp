32c22e64b3087fe982c9a333ef7d67c77af8799a
Santosh Marigowda <santosh.marigowda@sky.uk>
2024-11-01 18:07:39 +0000

    code to create EC2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# thecloudbootcamp
-The Cloud Bootcamp
+# The Cloud Bootcamp
+
+# Task: Provision an EC2 instance in AWS Cloud
+
+## Install and Configure AWS CLI
+
+## Run Terraform Commands
+
+- `terraform init`
+- `terraform plan`
+- `terraform apply`
---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = "1.9.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "example" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+}
