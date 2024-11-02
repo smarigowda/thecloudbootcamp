6dea0a498d1be5fe3f9daa6f3c5011d92eaeb3ed
Santosh Marigowda <santosh.marigowda@sky.uk>
2024-11-02 22:05:37 +0000

    code to create VPC in aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "example" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
-resource "aws_instance" "webserver" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t2.micro"
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
