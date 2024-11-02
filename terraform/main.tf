cbd864f0e153138dc50f974d490dd7cdd0a9628b
Santosh Marigowda <santosh.marigowda@abcam.com>
2024-11-02 11:49:12 +0000

    split terraform code into multiple files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = "1.9.8"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.74.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
 data "aws_ami" "example" {
   most_recent = true
   owners      = ["amazon"]
